e96a0c275fa2503eba51ed281830d8e827e6e91b
blacked <alexey.dubkov@gmail.com>
2014-03-19 17:47:52 +0400

    Now python version of script can push accumulated parameters to zabbix.

--- README.md
+++ README.md
@@ -11,7 +11,48 @@ Zabbix template for Redis (node.js or python)
 - [python](http://www.python.org/downloads/) 
 - [redis-py](https://github.com/andymccurdy/redis-py)
 
-##Install
+
+## Install
+You can monitor your redis in zabbix agent mode or through trap-messages.
+
+In zabbix agent mode, zabbix will periodically send request to an agent for every parameter, and agent will answer it.
+
+In trap-message mode, script will be periodically accumulate redis's parameters and will send it to zabbix as a one message.
+
+If you planning to capture many redis parameters and do it often. I would recomend  to use trap-message mode.
+
+Note: trap-message works only on python version of script
+
+### Install in trap-message mode
+
+1) Put `zbx_redis_stats.py` into your monitoring scripts path (like: `/etc/zabbix/script/redis/`).
+
+2) Change next section in zbx_redis_stats.py, to your configuration:
+'''
+zabbix_host = '127.0.0.1'	# Zabbix Server IP
+zabbix_port = 10051			# Zabbix Server Port
+hostname = 'redis.srv.name'	# Name of monitored server like it shows in zabbix web ui display
+redis_port = 6379			# Redis Server port
+'''
+
+3) In script path (`/etc/zabbix/script/redis/`) do:
+```
+pip install redis
+chmod +x zbx_redis_stats.py
+```
+
+4) Configure cron to run script every one minute:
+'''
+$ sudo crontab -e
+
+*/1  *    *   *     *       /etc/zabbix/script/nginx/zbx_redis_module.py
+'''
+
+5) Import `zbx_redis_trapper_template.xml` into zabbix in Tepmplate section web gui.
+
+That is all :)
+
+### Install in Zabbix Agent mode
 
 1) Put `zbx_redis.conf` into your `zabbix_agentd.conf` config subdirectory (like: `/etc/zabbix/zabbix_agentd.d/`).
 
--- zbx_redis_stats.py
+++ zbx_redis_stats.py
@@ -1,50 +1,131 @@
 #!/usr/bin/python
-#
-# This content is licensed GNU GPL v2
-# Author: Alexey Dubkov <alexey.dubkov@gmail.com>
-#
 
-import sys, redis, json, re
+import sys, redis, json, re, struct, time, socket
 
-host = (len(sys.argv) >= 2) and sys.argv[1] or 'localhost'
-port = 6379
-metric = (len(sys.argv) >= 3) and sys.argv[2]
-db = (len(sys.argv) >= 4) and sys.argv[3] or 'none'
+zabbix_host = '127.0.0.1'	# Zabbix Server IP
+zabbix_port = 10051			# Zabbix Server Port
+hostname = 'redis.srv.name'	# Name of monitored server like it shows in zabbix web ui display
+redis_port = 6379			# Redis Server port
 
-client = redis.StrictRedis(host=host, port=port)
-server_info = client.info()
+class Metric(object):
+    def __init__(self, host, key, value, clock=None):
+        self.host = host
+        self.key = key
+        self.value = value
+        self.clock = clock
 
-if metric:
-	if db and db in server_info.keys():
-		server_info['key_space_db_keys'] = server_info[db]['keys']
-		server_info['key_space_db_expires'] = server_info[db]['expires']
-		server_info['key_space_db_avg_ttl'] = server_info[db]['avg_ttl']
-	
-	def llen():
-		print client.llen(db)
+    def __repr__(self):
+        if self.clock is None:
+            return 'Metric(%r, %r, %r)' % (self.host, self.key, self.value)
+        return 'Metric(%r, %r, %r, %r)' % (self.host, self.key, self.value, self.clock)
 
-	def llensum():
-		keys = client.keys('*')
-		llensum = 0
-		for key in keys:
-			llensum += client.llen(key)
-		print llensum
+def send_to_zabbix(metrics, zabbix_host='127.0.0.1', zabbix_port=10051):
+    
+    j = json.dumps
+    metrics_data = []
+    for m in metrics:
+        clock = m.clock or ('%d' % time.time())
+        metrics_data.append(('{"host":%s,"key":%s,"value":%s,"clock":%s}') % (j(m.host), j(m.key), j(m.value), j(clock)))
+    json_data = ('{"request":"sender data","data":[%s]}') % (','.join(metrics_data))
+    data_len = struct.pack('<Q', len(json_data))
+    packet = 'ZBXD\x01'+ data_len + json_data
+    
+    #print packet
+    #print ':'.join(x.encode('hex') for x in packet)
 
-	def list_key_space_db():
-		if db in server_info:
-			print db
-		else:
-			print 'database_detect'
+    try:
+        zabbix = socket.socket()
+        zabbix.connect((zabbix_host, zabbix_port))
+        zabbix.sendall(packet)
+        resp_hdr = _recv_all(zabbix, 13)
+        if not resp_hdr.startswith('ZBXD\x01') or len(resp_hdr) != 13:
+            print 'Wrong zabbix response'
+            return False
+        resp_body_len = struct.unpack('<Q', resp_hdr[5:])[0]
+        resp_body = zabbix.recv(resp_body_len)
+        zabbix.close()
 
-	def default():
-		if metric in server_info.keys():
-			print server_info[metric]
+        resp = json.loads(resp_body)
+        #print resp
+        if resp.get('response') != 'success':
+            print 'Got error from Zabbix: %s' % resp
+            return False
+        return True
+    except:
+        print 'Error while sending data to Zabbix'
+        return False
 
-	{
-		'llen': llen,
-		'llenall': llensum,
-		'list_key_space_db': list_key_space_db,
-	}.get(metric, default)()
+
+def _recv_all(sock, count):
+    buf = ''
+    while len(buf)<count:
+        chunk = sock.recv(count-len(buf))
+        if not chunk:
+            return buf
+        buf += chunk
+    return buf
+
+
+
+if len(sys.argv) <= 2:
+	host = (len(sys.argv) >= 2) and sys.argv[1] or hostname
+
+	client = redis.StrictRedis(host=host, port=redis_port)
+	server_info = client.info()
+
+	a = []
+	for i in server_info:
+		a.append(Metric(host, ('redis[%s]' % i), server_info[i]))
+
+	keys = client.keys('*')
+	llensum = 0
+	for key in keys:
+		llensum += client.llen(key)
+	a.append(Metric(host, 'redis[llenall]', llensum))
+
+
+	send_to_zabbix(a, zabbix_host, zabbix_port)
 
 else:
-	print 'Not selected metric';
+	host = (len(sys.argv) >= 2) and sys.argv[1] or 'localhost'
+	metric = (len(sys.argv) >= 3) and sys.argv[2]
+	db = (len(sys.argv) >= 4) and sys.argv[3] or 'none'
+
+	client = redis.StrictRedis(host=host, port=redis_port)
+	server_info = client.info()
+
+	if metric:
+		if db and db in server_info.keys():
+			server_info['key_space_db_keys'] = server_info[db]['keys']
+			server_info['key_space_db_expires'] = server_info[db]['expires']
+			server_info['key_space_db_avg_ttl'] = server_info[db]['avg_ttl']
+		
+		def llen():
+			print client.llen(db)
+
+		def llensum():
+			keys = client.keys('*')
+			llensum = 0
+			for key in keys:
+				llensum += client.llen(key)
+			print llensum
+
+		def list_key_space_db():
+			if db in server_info:
+				print db
+			else:
+				print 'database_detect'
+
+		def default():
+			if metric in server_info.keys():
+				print server_info[metric]
+
+
+		{
+			'llen': llen,
+		 	'llenall': llensum,
+		 	'list_key_space_db': list_key_space_db,
+		}.get(metric, default)()
+
+	else:
+		print 'Not selected metric';
